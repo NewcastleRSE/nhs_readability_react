1988c78346eb3cb7e8d35cd2aba7320e89635546
David Herbert <david.herbert2@newcastle.ac.uk>
2022-08-04 17:13:47 +0100

    Tooltips

---
+++ src/assets/readability/PanelItems.js
@@ -0,0 +1,102 @@
+const switchListItems = [
+    {
+        key: 1,
+        id: 'show-complex-sentences',
+        primary: 'Show complex sentences',
+        help: 'Highlight sentences which are too complex and show potential alternatives',
+        defaultChecked: true
+    },
+    {
+        key: 2,
+        id: 'show-medical-terms',
+        primary: 'Highlight harder words',
+        help: 'Highlight words in the PRISM readability Toolkit having a simpler alternative word or phrase',
+        defaultChecked: true
+    },
+    {
+        key: 3,
+        id: 'include-medical-terms',
+        primary: 'Include medical terms in grading',
+        help: 'Compute the SMOG Index and Reading Age including all the medical terms',
+        defaultChecked: true
+    }
+];
+
+const metricListItems = [
+    {
+        key: 1,
+        id: 'nCharacters',
+        primary: 'Characters',
+        help: 'Total number of characters in document'
+    },
+    {
+        key: 2,
+        id: 'nSpaces',
+        primary: 'Whitespace',
+        help: 'Total number of whitespace characters (space, newline, tab) in document'
+    },
+    {
+        key: 3,
+        id: 'nWords',
+        primary: 'Words',
+        help: 'Total number of words in document'
+    },
+    {
+        key: 4,
+        id: 'nSentences',
+        primary: 'Sentences',
+        help: 'Total number of sentences in document'
+    },
+    {
+        key: 5,
+        id: 'nParagraphs',
+        primary: 'Paragraphs',
+        help: 'Total number of paragraphs in document'
+    }
+];
+
+const readabilityListItems = [
+    {
+        key: 1,
+        id: 'readingTime',
+        primary: 'Average reading time',
+        help: 'Estimated reading time for document, based on a 250 words per minute average'
+    },
+    {
+        key: 2,
+        id: 'smogIndex',
+        primary: 'SMOG Index',
+        help: `
+            <p>Returns the SMOG index of the given text. This is a grade formula in that a score of 9.3 means that a US ninth grader would be able to read the document.
+            Texts of fewer than 30 sentences are statistically invalid, because the SMOG formula was normed on 30-sentence samples</p>
+            Further reading on <a href="https://en.wikipedia.org/wiki/SMOG" target="_blank">Wikipedia</a>
+        `
+    },
+    {
+        key: 3,
+        id: 'ukReadingAge',
+        primary: 'Estimated UK Reading Age',
+        help: `
+            <p>The US grade system is translated into a UK school reading age via the following table:</p>
+            <table class="table is-fullwidth">
+                <tr><th>Year in<br/>England</th><th>Student age</th><th>US grade</th></tr>
+                <tr><td>Nursery</td><td>3-4</td><td>Preschool</td></tr>
+                <tr><td>Reception</td><td>4-5</td><td>Preschool</td></tr>
+                <tr><td>Year 2</td><td>6-7</td><td>Grade 1</td></tr>
+                <tr><td>Year 3</td><td>7-8</td><td>Grade 2</td></tr>
+                <tr><td>Year 4</td><td>8-9</td><td>Grade 3</td></tr>
+                <tr><td>Year 5</td><td>9-10</td><td>Grade 4</td></tr>
+                <tr><td>Year 6</td><td>10-11</td><td>Grade 5</td></tr>
+                <tr><td>Year 7</td><td>11-12</td><td>Grade 6</td></tr>
+                <tr><td>Year 8</td><td>12-13</td><td>Grade 7</td></tr>
+                <tr><td>Year 9</td><td>13-14</td><td>Grade 8</td></tr>
+                <tr><td>Year 10</td><td>14-15</td><td>Grade 9</td></tr>
+                <tr><td>Year 11</td><td>15-16</td><td>Grade 10</td></tr>
+                <tr><td>Year 12</td><td>16-17</td><td>Grade 11</td></tr>
+                <tr><td>Year 13</td><td>17-18</td><td>Grade 12</td></tr>
+            </table>                    
+        `
+    }
+];
+
+export { switchListItems, metricListItems, readabilityListItems };
--- src/components/DocumentAnalyser.js
+++ src/components/DocumentAnalyser.js
@@ -6,6 +6,11 @@ import { grey } from '@mui/material/colors';
 import Switch from '@mui/material/Switch';
 import { Help } from '@mui/icons-material';
 import TextModel from '../assets/readability/TextModel';
+import { switchListItems, metricListItems, readabilityListItems } from '../assets/readability/PanelItems';
+import tippy from 'tippy.js';
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+import 'tippy.js/themes/material.css';
 
 const darkGrey = grey.A700;
 const lightGrey = grey.A100;
@@ -76,137 +81,41 @@ const HelpListitem = styled(ListItemIcon)(({theme}) => ({
     color: theme.palette.info.dark
 }));
 
-const HelpIcon = () => {
+const HelpIcon = props => {
+    const { help, ...other } = props;
     return (
-        <HelpListitem><Help /></HelpListitem>
+        <HelpListitem>
+            <Help data-tippy-content={help} data-tippy-placement="top" />
+        </HelpListitem>
     );
 }
 
-const PanelListItem = props => {
-    const { id, primary } = props;
+const SwitchListItem = props => {
+    const { id, primary, help, defaultChecked } = props;
     const sx = { color: darkGrey, background: lightGrey };
     return (
         <ListItem>
-            <HelpIcon />
+            <HelpIcon help={help} />
             <ListItemText id={id} primary={primary} sx={sx} />
             <AntSwitch
                 color='default'
-                defaultChecked
+                defaultChecked={defaultChecked}
             />
         </ListItem>
     );
 };
 
 const MetricListItem = props => {
-    const { id, primary, value } = props;
+    const { id, primary, help, value } = props;
     const sx = { color: darkGrey, background: lightGrey };
     return (
         <ListItem>
-            <HelpIcon />
+            <HelpIcon help={help} />
             <ListItemText id={id} primary={primary} sx={sx} />
             <ListItemText primary={value} sx={ Object.assign({}, sx, { textAlign: 'right', paddingRight: '1em' }) } />
         </ListItem>
     );
 };
-
-const panelListItems = [
-    {
-        key: 1,
-        id: 'show-complex-sentences',
-        primary: 'Show complex sentences',
-        defaultChecked: true
-    },
-    {
-        key: 2,
-        id: 'show-medical-terms',
-        primary: 'Highlight medical terms',
-        defaultChecked: true
-    },
-    {
-        key: 3,
-        id: 'include-medical-terms',
-        primary: 'Include medical terms in grading',
-        defaultChecked: true
-    }
-];
-
-const metricListItems = [
-    {
-        key: 1,
-        id: 'nCharacters',
-        primary: 'Characters',
-        help: 'Total number of characters in document'
-    },
-    {
-        key: 2,
-        id: 'nSpaces',
-        primary: 'Whitespace',
-        help: 'Total number of whitespace characters (space, newline, tab) in document'
-    },
-    {
-        key: 3,
-        id: 'nWords',
-        primary: 'Words',
-        help: 'Total number of words in document'
-    },
-    {
-        key: 4,
-        id: 'nSentences',
-        primary: 'Sentences',
-        help: 'Total number of sentences in document'
-    },
-    {
-        key: 5,
-        id: 'nParagraphs',
-        primary: 'Paragraphs',
-        help: 'Total number of paragraphs in document'
-    }
-];
-
-const readabilityListItems = [
-    {
-        key: 1,
-        id: 'readingTime',
-        primary: 'Average reading time',
-        help: 'Estimated reading time for document, based on a 250 words per minute average'
-    },
-    {
-        key: 2,
-        id: 'smogIndex',
-        primary: 'SMOG Index',
-        help: `
-            <p>Returns the SMOG index of the given text. This is a grade formula in that a score of 9.3 means that a US ninth grader would be able to read the document.
-            Texts of fewer than 30 sentences are statistically invalid, because the SMOG formula was normed on 30-sentence samples</p>
-            Further reading on <a href="https://en.wikipedia.org/wiki/SMOG" target="_blank">Wikipedia</a>
-        `
-    },
-    {
-        key: 3,
-        id: 'ukReadingAge',
-        primary: 'Estimated UK Reading Age',
-        help: `
-            <p>The US grade system is translated into a UK school reading age via the following table:</p>
-            <table class="table is-fullwidth">
-                <tr><th>Year in<br/>England</th><th>Student age</th><th>US grade</th></tr>
-                <tr><td>Nursery</td><td>3-4</td><td>Preschool</td></tr>
-                <tr><td>Reception</td><td>4-5</td><td>Preschool</td></tr>
-                <tr><td>Year 2</td><td>6-7</td><td>Grade 1</td></tr>
-                <tr><td>Year 3</td><td>7-8</td><td>Grade 2</td></tr>
-                <tr><td>Year 4</td><td>8-9</td><td>Grade 3</td></tr>
-                <tr><td>Year 5</td><td>9-10</td><td>Grade 4</td></tr>
-                <tr><td>Year 6</td><td>10-11</td><td>Grade 5</td></tr>
-                <tr><td>Year 7</td><td>11-12</td><td>Grade 6</td></tr>
-                <tr><td>Year 8</td><td>12-13</td><td>Grade 7</td></tr>
-                <tr><td>Year 9</td><td>13-14</td><td>Grade 8</td></tr>
-                <tr><td>Year 10</td><td>14-15</td><td>Grade 9</td></tr>
-                <tr><td>Year 11</td><td>15-16</td><td>Grade 10</td></tr>
-                <tr><td>Year 12</td><td>16-17</td><td>Grade 11</td></tr>
-                <tr><td>Year 13</td><td>17-18</td><td>Grade 12</td></tr>
-            </table>                    
-        `
-    }
-];
-
 
 const textModel = new TextModel();
 
@@ -230,6 +139,17 @@ export default class DocumentAnalyser extends React.Component {
                 ukReadingAge: 0
             }
         };
+    }
+    componentDidMount() {
+        tippy.setDefaultProps({
+            theme: 'material',
+            arrow: true,
+            delay: [200, 200],
+            allowHTML: true,
+            interactive: true,
+            maxWidth: '20rem'
+        });
+        tippy('[data-tippy-content]');
     }
     render() {
         return (
@@ -259,22 +179,22 @@ export default class DocumentAnalyser extends React.Component {
                 <Grid item xs={12} sm={12} md={3}>
                     <WhitePaper elevation={5}>
                         <List sx={{ width: '100%' }} subheader={<PanelListSubheader caption="Document Analysis Options" />}>
-                            {panelListItems.map((pli) => (
-                                <PanelListItem key={pli.key} id={pli.id} primary={pli.primary} />
+                            {switchListItems.map((sli) => (
+                                <SwitchListItem key={sli.key} id={sli.id} primary={sli.primary} help={sli.help} defaultChecked={sli.defaultChecked} />
                             ))}
                         </List>
                     </WhitePaper>
                     <WhitePaper elevation={5}>
                         <List sx={{ width: '100%' }} subheader={<PanelListSubheader caption="Current Document Metrics" />}>
                             {metricListItems.map((mli) => (
-                                <MetricListItem key={mli.key} id={mli.id} primary={mli.primary} value={this.state.metrics[mli.id]} />
+                                <MetricListItem key={mli.key} id={mli.id} primary={mli.primary} help={mli.help} value={this.state.metrics[mli.id]} />
                             ))}
                         </List>
                     </WhitePaper>
                     <WhitePaper elevation={5}>
                         <List sx={{ width: '100%' }} subheader={<PanelListSubheader caption="Readability Metrics" />}>
                             {readabilityListItems.map((rli) => (
-                                <MetricListItem key={rli.key} id={rli.id} primary={rli.primary} value={this.state.readability[rli.id]} />
+                                <MetricListItem key={rli.key} id={rli.id} primary={rli.primary} help={rli.help} value={this.state.readability[rli.id]} />
                             ))}
                         </List>
                     </WhitePaper>
